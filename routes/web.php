e677c6fd104bb693936258f1fc4a818d9ba60cf5
Ahmed <ahmed@example.com>
2026-09-22 15:02:39 +0200

    Planeten route toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/planeten', function () {
+    return [
+        "Uranus", 
+        "Jupiter", 
+        "Mars", 
+        "Aarde", 
+        "Saturnus", 
+        "Pluto", 
+        "Neptunus", 
+        "Venus"
+    ];
 });
